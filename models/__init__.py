7385b818aab087dd4e4f845653a4214171f6a59f
Diego M. Navia Rojas <diego.navia@postgrado.uv.cl>
2026-05-24 21:49:21 -0400

    add: CriticalHiggsModel (Bezrukov) + EzquiagaCHIModel with phi->chi transform

--- models/__init__.py
+++ models/__init__.py
@@ -3,3 +3,4 @@ from .standard import QuadraticModel
 from .higgs import HiggsModel, FullHiggsModel
 from .Smooth_USR import SmoothUSRTransitionModel
 from .punctuated import PunctuatedInflationModel
+from .higgs_critical import CriticalHiggsModel
